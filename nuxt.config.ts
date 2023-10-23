a3835c195ebabe0568998617e93a1a36eca7df9d
iwairikuto <wjrikuto@gmial.com>
2023-10-23 23:06:45 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindTypography from "@tailwindcss/typography";
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["nuxt-microcms-module", "@nuxtjs/tailwindcss"],
+  microCMS: {
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+  },
+  tailwindcss: {
+    config: {
+      content: [],
+      plugins: [tailwindTypography],
+    },
+  },
+});
+// as anyでmicroCMSの型定義がないためエラーが出るので回避
